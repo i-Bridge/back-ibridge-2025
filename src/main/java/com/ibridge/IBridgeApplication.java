8b1bfc5f0c3cf769e8c0b56016402168ecaa53f8
Aqoom01 <rlagusgh789@daum.net>
2025-01-24 15:38:59 +0900

    style: 프로젝트 초기 설정

---
+++ src/main/java/com/ibridge/IBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.ibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibridge/IBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
