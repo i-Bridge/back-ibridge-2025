c3fb6cf9bd5a101fc20d418f87a6d17345c27173
woongjee <jeewoong7753@naver.com>
2025-02-12 15:48:36 +0900

    feat: 로그인 후 사용자 선택 화면 기능 구현

--- src/main/java/com/ibridge/controller/StartController.java
+++ src/main/java/com/ibridge/controller/StartController.java
@@ -1,6 +1,11 @@
 package com.ibridge.controller;
 
+import com.ibridge.domain.dto.response.UserSelectionResponseDTO;
+import com.ibridge.service.StartService;
+import com.ibridge.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/start")
 @RequiredArgsConstructor
 public class StartController {
+    private final StartService startService;
+
+    @GetMapping("/{accountId}/login")
+    public ApiResponse<UserSelectionResponseDTO> getUserSelection(@PathVariable("accountId") Long accountId) {
+        UserSelectionResponseDTO response = startService.getUserSelection(accountId);
+        return ApiResponse.onSuccess(response);
+    }
 }
