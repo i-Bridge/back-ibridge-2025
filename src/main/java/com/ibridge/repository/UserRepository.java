41bf283c8e3fe0e78b859d0c252794acb8fd2566
woongjee <jeewoong7753@naver.com>
2025-02-12 15:26:31 +0900

    feat: application.properties API KEY 환경 변수로 변경

---
+++ src/main/java/com/ibridge/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ibridge.repository;
+
+import com.ibridge.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
