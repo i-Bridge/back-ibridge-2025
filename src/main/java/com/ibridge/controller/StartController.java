f294f1a86a04563e301a1b27ee1b2d762ca1348b
woongjee <jeewoong7753@naver.com>
2025-02-26 13:22:41 +0900

    feat : 회원가입 시 가입된 가족이 없을 때 기능 구현

--- src/main/java/com/ibridge/controller/StartController.java
+++ src/main/java/com/ibridge/controller/StartController.java
@@ -1,11 +1,13 @@
 package com.ibridge.controller;
 
 import com.ibridge.domain.dto.request.StartRequestDTO;
+import com.ibridge.domain.dto.request.StartSignupNewRequestDTO;
 import com.ibridge.util.CustomOAuth2User;
 import com.ibridge.domain.dto.response.StartResponseDTO;
 import com.ibridge.service.StartService;
 import com.ibridge.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +26,16 @@ public class StartController {
         StartResponseDTO response = startService.signIn(oAuth2User);
         return ApiResponse.onSuccess(response);
     }
+
     @PostMapping("/signup/exist")
     public ApiResponse<StartResponseDTO> checkFamilyExistence(@RequestBody StartRequestDTO request){
         StartResponseDTO response = startService.checkFamilyExistence(request);
         return ApiResponse.onSuccess(response);
+    }
+
+    @PostMapping("/signup/new")
+    public ResponseEntity<ApiResponse<StartResponseDTO>> registerNewFamily(@RequestBody StartSignupNewRequestDTO request) {
+        StartResponseDTO response = startService.registerNewFamily(request);
+        return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 }
---
+++ src/main/java/com/ibridge/domain/dto/request/StartSignupNewRequestDTO.java
@@ -0,0 +1,32 @@
+package com.ibridge.domain.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class StartSignupNewRequestDTO {
+
+    @NotNull
+    private ParentRequest parent;
+
+    @NotNull
+    private List<ChildRequest> children;
+
+    @Getter
+    @NoArgsConstructor
+    public static class ParentRequest {
+        private String familyName;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class ChildRequest {
+        private String name;
+        private Integer gender;
+        private String birth;
+    }
+}
