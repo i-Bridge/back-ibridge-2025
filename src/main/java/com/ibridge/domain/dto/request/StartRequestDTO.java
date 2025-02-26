43a741eb1140eb0882b197c649e90098ab702b35
woongjee <jeewoong7753@naver.com>
2025-02-26 13:01:44 +0900

    feat : 회원가입 시 가입된 가족이 있을 때 기능 구현

--- src/main/java/com/ibridge/domain/dto/request/StartRequestDTO.java
+++ src/main/java/com/ibridge/domain/dto/request/StartRequestDTO.java
@@ -1,4 +1,12 @@
 package com.ibridge.domain.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Getter
+@NoArgsConstructor
 public class StartRequestDTO {
+    @NotNull
+    private String FamilyName;
 }
--- src/main/java/com/ibridge/repository/FamliyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ibridge.repository;
-
-import com.ibridge.domain.entity.Family;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface FamliyRepository extends JpaRepository<Family, Long> {
-}
