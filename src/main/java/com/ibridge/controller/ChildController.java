d87c47e2b281f5ce96dfcba13f87f3f0ad636b99
Aqoom01 <rlagusgh789@daum.net>
2025-03-04 07:52:14 +0900

    자식용 메인화면 조회

--- src/main/java/com/ibridge/controller/ChildController.java
+++ src/main/java/com/ibridge/controller/ChildController.java
@@ -1,6 +1,11 @@
 package com.ibridge.controller;
 
+import com.ibridge.domain.dto.response.ChildResponseDTO;
+import com.ibridge.service.ChildService;
+import com.ibridge.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ChildController {
 
+    private final ChildService childService;
+
+    @GetMapping("/{childId}/home")
+    public ApiResponse<ChildResponseDTO.getHomeDTO> home(@PathVariable Long childId) {
+        ChildResponseDTO.getHomeDTO data = childService.getHome(childId);
+        return ApiResponse.onSuccess(data);
+    }
 }
--- src/main/java/com/ibridge/domain/dto/response/ChildResponseDTO.java
+++ src/main/java/com/ibridge/domain/dto/response/ChildResponseDTO.java
@@ -1,4 +1,27 @@
 package com.ibridge.domain.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
 public class ChildResponseDTO {
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class getHomeDTO {
+        List<getQuestionsDTO> questions;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class getQuestionsDTO {
+        Long questionId;
+        boolean isAnswer;
+    }
+
 }
