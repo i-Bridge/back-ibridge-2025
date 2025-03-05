019fe3f2e32d7f3bac827b3267a92f88ab68b160
woongjee <jeewoong7753@naver.com>
2025-03-05 18:00:43 +0900

    fix: delete query

--- src/main/java/com/ibridge/repository/AnalysisRepository.java
+++ src/main/java/com/ibridge/repository/AnalysisRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
-    QuestionAnalysisDTO findAnalysisByQuestionId(Long parentId, Long questionId);
     Optional<Analysis> findByChildIdAndQuestionId(Long childId, Long questionId);
 }
