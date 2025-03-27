12047b669c65e49ec63f30b41d9e63573e6d991a
woongjee <jeewoong7753@naver.com>
2025-03-27 19:20:39 +0900

    fix : SecurityConfig 수정

--- src/main/java/com/ibridge/config/SecurityConfig.java
+++ src/main/java/com/ibridge/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ibridge.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,6 +11,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()  // 모든 요청 허용 (테스트용)
                         .anyRequest().authenticated()
