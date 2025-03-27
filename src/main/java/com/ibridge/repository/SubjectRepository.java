6851983a84950b23164b20ee3bdf5383dfba0957
woongjee <jeewoong7753@naver.com>
2025-03-27 19:30:46 +0900

    fix : SubjectRepository 수정

--- src/main/java/com/ibridge/repository/SubjectRepository.java
+++ src/main/java/com/ibridge/repository/SubjectRepository.java
@@ -2,11 +2,13 @@ package com.ibridge.repository;
 
 import com.ibridge.domain.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Date;
 import java.util.List;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     List<Subject> findByChildId(Long childId);
+    @Query("SELECT s FROM Subject s where s.child.id = :childId")
     Subject findSubjectByChildId(Long childId);
 }
