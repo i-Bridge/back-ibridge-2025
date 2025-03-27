409477b1719b7a4550d819fd2a13fc4610dd6d4d
woongjee <jeewoong7753@naver.com>
2025-03-27 20:39:38 +0900

    fix : StartService 수정

--- src/main/java/com/ibridge/domain/dto/request/StartRequestDTO.java
+++ src/main/java/com/ibridge/domain/dto/request/StartRequestDTO.java
@@ -1,11 +1,13 @@
 package com.ibridge.domain.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class StartRequestDTO {
     private String FamilyName;
 }
