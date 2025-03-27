6f031f624d203193362837f5e7a55cceb1aff1cd
Aqoom01 <rlagusgh789@daum.net>
2025-03-27 20:49:40 +0900

    fix

--- src/main/java/com/ibridge/domain/dto/request/StartRequestDTO.java
+++ src/main/java/com/ibridge/domain/dto/request/StartRequestDTO.java
@@ -1,11 +1,13 @@
 package com.ibridge.domain.dto.request;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
+@Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class StartRequestDTO {
     @NotNull
     private String FamilyName;
