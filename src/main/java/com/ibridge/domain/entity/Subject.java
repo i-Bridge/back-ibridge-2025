3a78f6304056476e79ed64ca314cb555c79fdfc1
woongjee <jeewoong7753@naver.com>
2025-03-27 16:51:53 +0900

    feat : subject 엔티티 수정

--- src/main/java/com/ibridge/domain/entity/Subject.java
+++ src/main/java/com/ibridge/domain/entity/Subject.java
@@ -32,6 +32,6 @@ public class Subject {
     @JoinColumn(name = "child")
     private Child child;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL)
     private List<Question> questions;
 }
