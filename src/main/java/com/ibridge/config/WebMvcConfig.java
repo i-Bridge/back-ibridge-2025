0501d565b52d0650a8b04560ec4a03b65a5d0ee1
woongjee <jeewoong7753@naver.com>
2025-05-10 17:41:52 +0900

    fix

--- src/main/java/com/ibridge/config/WebMvcConfig.java
+++ src/main/java/com/ibridge/config/WebMvcConfig.java
@@ -16,6 +16,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenValidationInterceptor)
-                .addPathPatterns("/**"); // 인증 필요한 경로만 걸기
+                .addPathPatterns("/start/*", "/parent/*"); // 인증 필요한 경로만 걸기
     }
 }
