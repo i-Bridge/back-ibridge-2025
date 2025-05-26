88d589f174ff49166e530d935c279d65b375a9a3
Aqoom01 <rlagusgh789@daum.net>
2025-05-26 14:01:05 +0900

    fix: 시간대 설정

--- src/main/java/com/ibridge/IBridgeApplication.java
+++ src/main/java/com/ibridge/IBridgeApplication.java
@@ -3,11 +3,13 @@ package com.ibridge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class IBridgeApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(IBridgeApplication.class, args);
     }
 
