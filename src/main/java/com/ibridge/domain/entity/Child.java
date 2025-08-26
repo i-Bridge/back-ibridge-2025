5570ae67c16eac409dcafcb853c15e108a31917c
Aqoom01 <rlagusgh789@daum.net>
2025-08-26 12:40:45 +0900

    fix: 엔티티 기본 값 설정

--- src/main/java/com/ibridge/domain/entity/Child.java
+++ src/main/java/com/ibridge/domain/entity/Child.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -36,5 +37,6 @@ public class Child {
     private List<Subject> subjects;
 
     @Column(nullable = false)
+    @ColumnDefault("0")
     private Long grape;
 }
