1cdb671e94f15b32a9d8b5cc82e821b7069a9075
Aqoom01 <rlagusgh789@daum.net>
2025-09-20 23:33:27 +0900

    fix: 엔티티 수정에 따른 레포지토리 수정

--- src/main/java/com/ibridge/repository/ChildPositiveBoardRepository.java
+++ src/main/java/com/ibridge/repository/ChildPositiveBoardRepository.java
@@ -14,28 +14,28 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ChildPositiveBoardRepository extends JpaRepository<ChildPositiveBoard, Long> {
-    @Query("SELECT new com.ibridge.domain.dto.response.KeywordDTO(cb.keyword, cb.keywordCount,cb.positive) " +
-            "FROM ChildPositiveBoard cb " +
-            "WHERE cb.child = :child AND cb.type = com.ibridge.domain.entity.PeriodType.MONTH " +
-            "AND cb.period = :periodValue " +
-            "ORDER BY cb.keywordCount DESC")
-    List<KeywordDTO> findKeywordsAndPositivesByMonth(
-            @Param("child") Child child,
-            Pageable pageable);
-
-    @Query("SELECT cb " +
-            "FROM ChildPositiveBoard cb " +
-            "WHERE cb.child = :child AND cb.type = :periodType AND cb.period = :periodValue " +
-            "ORDER BY cb.period DESC")
-    List<ChildPositiveBoard> findByChildAndTypeAndPeriod(Child child, PeriodType periodType, String periodValue, Pageable pageable);
-
-    @Query("SELECT cb " +
-            "FROM ChildPositiveBoard cb " +
-            "WHERE cb.child = :child " +
-            "AND cb.type = :periodType " +
-            "AND cb.period = :period " +
-            "AND cb.keyword = :keyword")
-    Optional<ChildPositiveBoard> findByKeywordandChildwithDatewithType(String keyword, Child child, String period, PeriodType periodType);
+//    @Query("SELECT new com.ibridge.domain.dto.response.KeywordDTO(cb.keyword, cb.keywordCount,cb.positive) " +
+//            "FROM ChildPositiveBoard cb " +
+//            "WHERE cb.child = :child AND cb.type = com.ibridge.domain.entity.PeriodType.MONTH " +
+//            "AND cb.period = :periodValue " +
+//            "ORDER BY cb.keywordCount DESC")
+//    List<KeywordDTO> findKeywordsAndPositivesByMonth(
+//            @Param("child") Child child,
+//            Pageable pageable);
+//
+//    @Query("SELECT cb " +
+//            "FROM ChildPositiveBoard cb " +
+//            "WHERE cb.child = :child AND cb.type = :periodType AND cb.period = :periodValue " +
+//            "ORDER BY cb.period DESC")
+//    List<ChildPositiveBoard> findByChildAndTypeAndPeriod(Child child, PeriodType periodType, String periodValue, Pageable pageable);
+//
+//    @Query("SELECT cb " +
+//            "FROM ChildPositiveBoard cb " +
+//            "WHERE cb.child = :child " +
+//            "AND cb.type = :periodType " +
+//            "AND cb.period = :period " +
+//            "AND cb.keyword = :keyword")
+//    Optional<ChildPositiveBoard> findByKeywordandChildwithDatewithType(String keyword, Child child, String period, PeriodType periodType);
 
     @Query("SELECT new com.ibridge.domain.dto.response.KeywordDTO(c.keyword, c.keywordCount, c.positive) " +
             "FROM ChildPositiveBoard c " +
